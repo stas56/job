04d5c411e83f91c5c0fe595d51290facc9807b2d
stasok <orenuc@mail.ru>
2013-06-16 18:15:43 +0600

    init job-project

---
+++ Views/footer.php
@@ -0,0 +1,10 @@
+
+		</div>
+	</div>	
+    <footer>
+		<div class="wrapper">
+			Служба технической поддержки пользователей<img src="/Resource/gfx/logo-nettuts.png">
+		</div>
+	</footer>
+</body>
+</html>
